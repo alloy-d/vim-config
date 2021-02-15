212a0a11e41eead367e490a649549fb089a2e309
Adam Lloyd <adam@alloy-d.net>
2021-02-15 10:03:59 -0500

    When updating packages, use origin/HEAD instead of origin/master.

--- update-packages.sh
+++ update-packages.sh
@@ -2,5 +2,5 @@
 set -euxo pipefail
 
 git submodule foreach git fetch origin
-git submodule foreach git checkout origin/master
+git submodule foreach git checkout origin/HEAD
 git submodule foreach git submodule update --init --recursive
